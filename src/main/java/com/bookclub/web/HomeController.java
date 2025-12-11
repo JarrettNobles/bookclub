c3131b22ba557e3bb2c1f96824cc30f6103167d6
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-12-11 13:37:31 -0500

    Add comments

--- src/main/java/com/bookclub/web/HomeController.java
+++ src/main/java/com/bookclub/web/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//home controller
 @Controller
 @RequestMapping("/")
 public class HomeController {
